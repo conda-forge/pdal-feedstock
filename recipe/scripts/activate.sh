5e9fe2f660ec67570d15f3f9bfbb5a501950726e
Bradley J Chambers <brad.chambers@gmail.com>
2018-04-24 06:04:03 -0400

    Remove check for existence of gdal directory

--- recipe/scripts/activate.sh
+++ recipe/scripts/activate.sh
@@ -7,11 +7,7 @@ if [[ -n "$PDAL_DRIVER_PATH" ]]; then
     export _CONDA_SET_PDAL_DRIVER_PATH=$PDAL_DRIVER_PATH
 fi
 
-if [ -d $CONDA_PREFIX/share/gdal ]; then
-    export PDAL_DRIVER_PATH=$CONDA_PREFIX/lib
-elif [ -d $CONDA_PREFIX/Library/share/gdal ]; then
-    export PDAL_DRIVER_PATH=$CONDA_PREFIX/Library/lib
-fi
+export PDAL_DRIVER_PATH=$CONDA_PREFIX/lib
 
 # Support plugins if the plugin directory exists
 # i.e if it has been manually created by the user
