e768fed5fdc6cb810b9673c618ce35e25471172a
Bradley J Chambers <brad.chambers@gmail.com>
2018-04-25 06:49:52 -0400

    Correct the windows PDAL_DRIVER_PATH

--- recipe/scripts/activate.bat
+++ recipe/scripts/activate.bat
@@ -7,7 +7,7 @@
 
 @REM Support plugins if the plugin directory exists
 @REM i.e if it has been manually created by the user
-@set "PDAL_DRIVER_PATH=%CONDA_PREFIX%\lib"
+@set "PDAL_DRIVER_PATH=%CONDA_PREFIX%\Library\lib"
 @if not exist %PDAL_DRIVER_PATH% (
      set "PDAL_DRIVER_PATH="
 )
