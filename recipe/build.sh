92eb4fb6c042ae19813bd9e6283b64a86e10c4b8
Howard Butler <howard@hobu.co>
2019-08-23 14:28:48 -0500

    keep swinging at python

--- recipe/build.sh
+++ recipe/build.sh
@@ -7,16 +7,19 @@ if [ "$(uname)" == "Linux" ]; then
    export LDFLAGS="${LDFLAGS} -Wl,-rpath-link,${PREFIX}/lib"
 fi
 
-which python
+echo "Python verison:"
+`which python` -V
 
+mkdir build
+cd build
 cmake -G "Unix Makefiles" \
   -DCMAKE_BUILD_TYPE=Release \
   -DCMAKE_INSTALL_PREFIX=$PREFIX \
   -DCMAKE_LIBRARY_PATH=$PREFIX/lib \
   -DCMAKE_INCLUDE_PATH=$PREFIX/include \
   -DBUILD_PLUGIN_I3S=ON \
+  -DPython3_EXECUTABLE:FILEPATH=$BUILD_PREFIX/bin/python \
   -DBUILD_PLUGIN_PYTHON=ON \
-  -DPython3_EXECUTABLE:FILEPATH="$PREFIX/bin/python" \
   -DBUILD_PLUGIN_PGPOINTCLOUD=ON \
   -DBUILD_PLUGIN_SQLITE=ON \
   -DBUILD_PLUGIN_ICEBRIDGE=ON \
@@ -27,7 +30,8 @@ cmake -G "Unix Makefiles" \
   -DWITH_ZLIB=ON \
   -DWITH_ZSTD=ON \
   -DWITH_LAZPERF=ON \
-  -DWITH_LASZIP=ON
+  -DWITH_LASZIP=ON \
+  ..
 
 # CircleCI offers two cores.
 make -j $CPU_COUNT
