d81b5e93f4bd058bd6fc755229a9ebbf10407f77
Howard Butler <howard@hobu.co>
2019-08-23 13:57:09 -0500

    random things tried randomly

--- recipe/build.sh
+++ recipe/build.sh
@@ -7,6 +7,8 @@ if [ "$(uname)" == "Linux" ]; then
    export LDFLAGS="${LDFLAGS} -Wl,-rpath-link,${PREFIX}/lib"
 fi
 
+which python
+
 cmake -G "Unix Makefiles" \
   -DCMAKE_BUILD_TYPE=Release \
   -DCMAKE_INSTALL_PREFIX=$PREFIX \
@@ -14,7 +16,7 @@ cmake -G "Unix Makefiles" \
   -DCMAKE_INCLUDE_PATH=$PREFIX/include \
   -DBUILD_PLUGIN_I3S=ON \
   -DBUILD_PLUGIN_PYTHON=ON \
-  -DPython3_EXECUTABLE:FILEPATH="$BUILD_PREFIX/bin/python" \
+  -DPython3_EXECUTABLE:FILEPATH="$PREFIX/bin/python" \
   -DBUILD_PLUGIN_PGPOINTCLOUD=ON \
   -DBUILD_PLUGIN_SQLITE=ON \
   -DBUILD_PLUGIN_ICEBRIDGE=ON \
