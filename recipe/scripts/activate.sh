b5521e71f7c3435de73b0b8551ce07d16d10da83
Bradley J Chambers <brad.chambers@gmail.com>
2021-01-11 10:59:45 -0600

    Fix activate/deactivate scripts on *nix

--- recipe/scripts/activate.sh
+++ recipe/scripts/activate.sh
@@ -1,9 +1,7 @@
-#!/bin/bash
-
 # Store existing PDAL env vars and set to this conda env
 # so other PDAL installs don't pollute the environment
 
-if [[ -n "$PDAL_DRIVER_PATH" ]]; then
+if [ -n "$PDAL_DRIVER_PATH" ]; then
     export _CONDA_SET_PDAL_DRIVER_PATH=$PDAL_DRIVER_PATH
 fi
 
@@ -11,7 +9,7 @@ export PDAL_DRIVER_PATH=$CONDA_PREFIX/lib
 
 # Support plugins if the plugin directory exists
 # i.e if it has been manually created by the user
-if [[ ! -d "$PDAL_DRIVER_PATH" ]]; then
+if [ ! -d "$PDAL_DRIVER_PATH" ]; then
     unset PDAL_DRIVER_PATH
 fi
 
