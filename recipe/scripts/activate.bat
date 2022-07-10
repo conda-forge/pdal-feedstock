3d466d6c6e10ffe2cc3d94337c07cd0614dee270
Ogi Moore <ognyan.moore@gmail.com>
2022-07-10 07:14:21 -0700

    Handle paths with spaces in activate.bat (#184)
    * Set the correct path for PDAL_DRIVER_PATH on powershell
    
    * Handle spaces in paths used as environment variables
    
    * Bump build number
    
    * MNT: Re-rendered with conda-build 3.21.9, conda-smithy 3.21.0, and conda-forge-pinning 2022.07.06.01.52.02

--- recipe/scripts/activate.bat
+++ recipe/scripts/activate.bat
@@ -8,7 +8,7 @@
 @REM Support plugins if the plugin directory exists
 @REM i.e if it has been manually created by the user
 @set "PDAL_DRIVER_PATH=%CONDA_PREFIX%\Library\bin"
-@if not exist %PDAL_DRIVER_PATH% (
+@if not exist "%PDAL_DRIVER_PATH%" (
      set "PDAL_DRIVER_PATH="
 )
 
--- recipe/scripts/activate.sh
+++ recipe/scripts/activate.sh
@@ -2,10 +2,10 @@
 # so other PDAL installs don't pollute the environment
 
 if [ -n "$PDAL_DRIVER_PATH" ]; then
-    export _CONDA_SET_PDAL_DRIVER_PATH=$PDAL_DRIVER_PATH
+    export _CONDA_SET_PDAL_DRIVER_PATH="$PDAL_DRIVER_PATH"
 fi
 
-export PDAL_DRIVER_PATH=$CONDA_PREFIX/lib
+export PDAL_DRIVER_PATH="$CONDA_PREFIX/lib"
 
 # Support plugins if the plugin directory exists
 # i.e if it has been manually created by the user
