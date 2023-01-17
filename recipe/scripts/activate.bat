884303af2bbc029822cd8cfbed261e3af4760104
ClementAlba <93247842+ClementAlba@users.noreply.github.com>
2023-01-17 16:00:55 +0100

    Update activate.bat

--- recipe/scripts/activate.bat
+++ recipe/scripts/activate.bat
@@ -7,7 +7,7 @@
 
 @REM Support plugins if the plugin directory exists
 @REM i.e if it has been manually created by the user
-@set "PDAL_DRIVER_PATH=%CONDA_PREFIX%\Library\bin"
+@set "PDAL_DRIVER_PATH=%CONDA_PREFIX%\Library\bin;%CONDA_PREFIX%\bin"
 @if not exist "%PDAL_DRIVER_PATH%" (
      set "PDAL_DRIVER_PATH="
 )
