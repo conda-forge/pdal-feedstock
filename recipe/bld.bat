54d019319cf1208c68a040f0a71cba7e38b86970
Howard Butler <hobu.inc@gmail.com>
2023-08-25 09:28:34 -0500

    working locally for me

--- recipe/bld.bat
+++ recipe/bld.bat
@@ -24,25 +24,3 @@ if errorlevel 1 exit 1
 
 nmake
 if errorlevel 1 exit 1
-
-nmake install
-if errorlevel 1 exit 1
-
-set ACTIVATE_DIR=%PREFIX%\etc\conda\activate.d
-set DEACTIVATE_DIR=%PREFIX%\etc\conda\deactivate.d
-mkdir %ACTIVATE_DIR%
-mkdir %DEACTIVATE_DIR%
-
-copy %RECIPE_DIR%\scripts\activate.bat %ACTIVATE_DIR%\pdal-activate.bat
-if errorlevel 1 exit 1
-
-copy %RECIPE_DIR%\scripts\deactivate.bat %DEACTIVATE_DIR%\pdal-deactivate.bat
-if errorlevel 1 exit 1
-
-:: Copy unix shell activation scripts, needed by Windows Bash users
-copy %RECIPE_DIR%\scripts\activate.sh %ACTIVATE_DIR%\pdal-activate.sh
-if errorlevel 1 exit 1
-
-copy %RECIPE_DIR%\scripts\deactivate.sh %DEACTIVATE_DIR%\pdal-deactivate.sh
-if errorlevel 1 exit 1
-
--- recipe/build.sh
+++ recipe/build.sh
@@ -58,15 +58,4 @@ cmake ${CMAKE_ARGS} \
   ..
 
 make -j $CPU_COUNT ${VERBOSE_CM}
-make install
 
-# This will not be needed once we fix upstream.
-chmod 755 $PREFIX/bin/pdal-config
-
-ACTIVATE_DIR=$PREFIX/etc/conda/activate.d
-DEACTIVATE_DIR=$PREFIX/etc/conda/deactivate.d
-mkdir -p $ACTIVATE_DIR
-mkdir -p $DEACTIVATE_DIR
-
-cp $RECIPE_DIR/scripts/activate.sh $ACTIVATE_DIR/pdal-activate.sh
-cp $RECIPE_DIR/scripts/deactivate.sh $DEACTIVATE_DIR/pdal-deactivate.sh
