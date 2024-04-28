67d4de78231a9cef806bc32453d89814878e0c9d
Howard Butler <hobu.inc@gmail.com>
2024-04-28 12:26:44 -0500

    install scripts for libpdal-python

--- recipe/install_pdal_python.bat
+++ recipe/install_pdal_python.bat
@@ -28,3 +28,23 @@ popd
 
 pdal-python path
 popd
+
+
+set "ACTIVATE_DIR=%PREFIX%\etc\conda\activate.d"
+set "DEACTIVATE_DIR=%PREFIX%\etc\conda\deactivate.d"
+
+REM because we set ACTIVATE_DIR in this conditional, we have to
+REM use the !
+mkdir !ACTIVATE_DIR!
+mkdir !DEACTIVATE_DIR!
+
+
+:: Copy the [de]activate scripts to %PREFIX%\etc\conda\[de]activate.d.
+:: This will allow them to be run on environment activation.
+for %%F in (activate deactivate) DO (
+    if not exist %PREFIX%\etc\conda\%%F.d mkdir %PREFIX%\etc\conda\%%F.d
+    copy %RECIPE_DIR%\%%F-%PKG_NAME%.bat %PREFIX%\etc\conda\%%F.d\%PKG_NAME%_%%F.bat
+    copy %RECIPE_DIR%\%%F-%PKG_NAME%.ps1 %PREFIX%\etc\conda\%%F.d\%PKG_NAME%_%%F.ps1
+    :: Copy unix shell activation scripts, needed by Windows Bash users
+    copy %RECIPE_DIR%\%%F-%PKG_NAME%.sh %PREFIX%\etc\conda\%%F.d\%PKG_NAME%_%%F.sh
+)
--- recipe/install_pdal_python.sh
+++ recipe/install_pdal_python.sh
@@ -34,3 +34,13 @@ ${PYTHON} -m pip install .  -vv --no-deps --no-build-isolation  --upgrade
 popd
 
 popd
+
+
+# Copy the [de]activate scripts to $PREFIX/etc/conda/[de]activate.d.
+# This will allow them to be run on environment activation.
+for CHANGE in "activate" "deactivate"
+do
+    mkdir -p "${PREFIX}/etc/conda/${CHANGE}.d"
+    echo "reading data in ${RECIPE_DIR}"
+    cp "${RECIPE_DIR}/scripts/${CHANGE}-${PKG_NAME}.sh" "${PREFIX}/etc/conda/${CHANGE}.d/${PKG_NAME}_${CHANGE}.sh"
+done
