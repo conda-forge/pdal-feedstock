65a7c20681f3d3b19119e58826f752cbccfe8cf1
Howard Butler <hobu.inc@gmail.com>
2024-04-29 22:18:04 -0500

    use released versions for python bindings and plugins

--- recipe/install_pdal_python.bat
+++ recipe/install_pdal_python.bat
@@ -1,33 +1,13 @@
 @echo on
 setlocal ENABLEDELAYEDEXPANSION
 
-
-git clone https://github.com/PDAL/python.git pdal-python
-pushd pdal-python
-
-
 :: set UNIX_SP_DIR=%SP_DIR:\=/%
 @REM set CMAKE_ARGS=%CMAKE_ARGS% -DPDAL_DIR=$PREFIX -LAH --debug-find -DPYTHON3_NUMPY_INCLUDE_DIRS=%UNIX_SP_DIR%/numpy/core/include
 
-%PYTHON% -m pip install . -vv --no-deps --no-build-isolation
 
-mkdir plugins
-pushd plugins
-curl -OL https://files.pythonhosted.org/packages/a3/0a/65e7114cae766ffcfa94c31413197acd35213fd6a3461f656b8ff967a75a/pdal_plugins-1.5.0.tar.gz
-tar xvf pdal_plugins-1.5.0.tar.gz
-pushd pdal_plugins-1.5.0
+%PYTHON% -m pip install pdal==%PDAL_PYTHON_BINDINGS_VERSION% -vv --no-deps --no-build-isolation  --upgrade
 
-
-%PYTHON% -m pip install . -vv --no-deps --no-build-isolation
-
-@REM pdal_plugins path
-popd
-
-@REM plugins path
-popd
-
-pdal-python path
-popd
+%PYTHON% -m pip install pdal-plugins==%PDAL_PYTHON_PLUGINS_VERSION%  -vv --no-deps --no-build-isolation  --upgrade
 
 
 set "ACTIVATE_DIR=%PREFIX%\etc\conda\activate.d"
