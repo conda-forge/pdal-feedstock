bc6200bc7923d41983d8355ac2cc3d56ec1f3c48
Howard Butler <hobu.inc@gmail.com>
2024-04-30 11:23:18 -0500

    conditionalize activate script for libpdal-python on whether or not BUILD_PREFIX is in the environment

--- recipe/scripts/activate-libpdal-python.sh
+++ recipe/scripts/activate-libpdal-python.sh
@@ -7,19 +7,16 @@ if [[ -n "$PDAL_DRIVER_PATH" ]]; then
     export _CONDA_SET_PDAL_PYTHON_DRIVER_PATH=$PDAL_DRIVER_PATH
 fi
 
-env
-# PLUGIN_DIR_PATH=$(python -m pdal --pdal-plugin-path)
-
-# if [[ ! -z "$PLUGIN_DIR_PATH" ]]; then
-#     export PDAL_DRIVER_PATH=$_CONDA_SET_PDAL_PYTHON_DRIVER_PATH:$PLUGIN_DIR_PATH
-# fi
-
-SITE_PACKAGES=$(python -c 'import site; print(site.getsitepackages()[0])')
-SITE_PACKAGES_PDAL=$(python -c 'import site; import os.path; site_packages_pdal = os.path.join(site.getsitepackages()[0], "pdal"); print( site_packages_pdal if os.path.exists(site_packages_pdal) else "")')
-
-if [[ ! -z "$SITE_PACKAGES_PDAL" ]]; then
-    export PDAL_DRIVER_PATH=$_CONDA_SET_PDAL_PYTHON_DRIVER_PATH:$SITE_PACKAGES/lib:$SITE_PACKAGES/lib64:$SITE_PACKAGES_PDAL
-else
-    export PDAL_DRIVER_PATH=$_CONDA_SET_PDAL_PYTHON_DRIVER_PATH:$SITE_PACKAGES/lib:$SITE_PACKAGES/lib64
+# if there's a BUILD_PREFIX we might be running under the host python
+# and get an "Bad CPU type in executable"
+if [ -z "$BUILD_PREFIX" ]; then
+    PLUGIN_DIR_PATH=$(python -m pdal --pdal-plugin-path)
 fi
+
+
+if [[ ! -z "$PLUGIN_DIR_PATH" ]]; then
+    export PDAL_DRIVER_PATH=$_CONDA_SET_PDAL_PYTHON_DRIVER_PATH:$PLUGIN_DIR_PATH
+fi
+
+
 
